d1f8c48ffdddfcb9566e1849e98a2419667c4139
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-11-08 14:38:38 +0100

    fixed Comment model table

--- src/classes/mediaApp/model/Comment.php
+++ src/classes/mediaApp/model/Comment.php
@@ -2,16 +2,18 @@
 
 namespace iutnc\mediaApp\model;
 
-class Comment extends \Illuminate\Database\Eloquent\Model{
+class Comment extends \Illuminate\Database\Eloquent\Model
+{
 
-    protected $table      = 'comment';  /* le nom de la table */
+    protected $table      = 'atelier1__comment';  /* le nom de la table */
     protected $primaryKey = 'id';     /* le nom de la clé primaire */
     protected $fillable = ['content', 'image_id', 'user_id'];
     public    $timestamps = true;    /* si vrai la table doit contenir
                                          les deux colonnes updated_at,
                                          created_at */
-                               
-    public function user() {
-    return $this->belongsTo('\iutnc\tweeterapp\model\user', 'user_id');
+
+    public function user()
+    {
+        return $this->belongsTo('\iutnc\tweeterapp\model\user', 'user_id');
     }
 }
