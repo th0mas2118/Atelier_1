1b9016575f08e718310159405ece1198fbb546cb
th0mas2118 <thomaspierson32@gmail.com>
2022-11-08 14:04:40 +0100

    fix synthax

--- src/classes/mediaApp/model/User.php
+++ src/classes/mediaApp/model/User.php
@@ -3,12 +3,12 @@
 namespace iutnc\mediaApp\model;
 
 class User extends \Illuminate\Database\Eloquent\Model{
-    protected $table='user';
+    protected $table='atelier1__user';
     protected $primarykey='id';
     public $timestamps=false;
 
 
-    public function galeries(){
-        return $this->hasMAny('\iutnc\meadiaApp\model\Galery','author');
+    public function galleries(){
+        return $this->hasMany('\iutnc\meadiaApp\model\Gallery','author');
     }
 }
