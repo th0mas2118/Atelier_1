c84b54dc4981e98f3372699280ee7b718ced0a55
th0mas2118 <thomaspierson32@gmail.com>
2022-11-08 14:09:10 +0100

    fillable

--- src/classes/mediaApp/model/User.php
+++ src/classes/mediaApp/model/User.php
@@ -5,6 +5,7 @@ namespace iutnc\mediaApp\model;
 class User extends \Illuminate\Database\Eloquent\Model{
     protected $table='atelier1__user';
     protected $primarykey='id';
+    protected $fillable=['username', 'fullname', 'password' , 'user_level'];
     public $timestamps=false;
 
 
