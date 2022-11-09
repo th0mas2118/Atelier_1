32e7fc59699ee8939bfdaaf26be97059dae1666d
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-11-09 10:51:45 +0100

    added authentification class and finished signup

--- src/classes/mediaApp/control/SignUpController.php
+++ src/classes/mediaApp/control/SignUpController.php
@@ -2,14 +2,42 @@
 
 namespace iutnc\mediaApp\control;
 
+use iutnc\mediaApp\auth\Authentification;
 use \iutnc\mf\control\AbstractController;
 use \iutnc\mediaApp\view\SignUpView;
+use iutnc\mf\router\Router;
 
-class SignUpController extends AbstractController{
-    public function execute():void{
-        if($this->request->method==='GET'){
-            $sv=new SignUpView();
+class SignUpController extends AbstractController
+{
+    public function execute(): void
+    {
+        if ($this->request->method === 'GET') {
+            $sv = new SignUpView();
             $sv->makePage();
+        }
+
+        if ($this->request->method === 'POST') {
+            $username = $this->request->post['username'];
+            $fullname = $this->request->post['fullname'];
+            $password = $this->request->post['password'];
+            $passwordConfirmation = $this->request->post['password-verify'];
+
+            if (empty($username) || empty($fullname) || empty($password) || empty($passwordConfirmation)) {
+                echo "Veuillez remplir tout les champs";
+                return;
+            }
+
+            if ($password !== $passwordConfirmation) {
+                echo "Les mots de passes ne correspondent pas";
+                return;
+            }
+
+            try {
+                Authentification::register($username, $password, $fullname);
+                Router::executeRoute("home");
+            } catch (\Throwable $th) {
+                throw new $th;
+            }
         }
     }
 }
--- src/classes/mediaApp/view/SignUpView.php
+++ src/classes/mediaApp/view/SignUpView.php
@@ -4,15 +4,17 @@ namespace iutnc\mediaApp\view;
 
 use \iutnc\mf\view\Renderer;
 
-class SignUpView extends MainView implements Renderer{
-    public function render():string{
-        $res=<<<EOT
+class SignUpView extends MainView implements Renderer
+{
+    public function render(): string
+    {
+        $res = <<<EOT
             <section>
                 <form method=post>
-                    <input type=text name=fullname placeholder="full name">
-                    <input type=text name=username placeholder="username">
-                    <input type=password name=password placeholder="password">
-                    <input type=password name=password-verify placeholder="retype password">
+                    <input type="text" name="fullname" placeholder="full name">
+                    <input type="text" name="username" placeholder="username">
+                    <input type="password" name="password" placeholder="password">
+                    <input type="password" name="password-verify" placeholder="retype password">
 
                     <button class="forms-button" name=sign_button type="submit">Create</button>
                 </form>
