50c600e088740ca33d0a230eff2dd43c9b7d88f4
Asley <asleyvalisoa@gmail.com>
2022-11-09 09:32:51 +0200

    Create GalleryView.php

---
+++ src/classes/mediaApp/view/GalleryView.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace iutnc\mediaApp\view;
+
+use iutnc\mediaApp\view\MainView;
+use iutnc\mf\view\Renderer;
+
+class GalleryView extends MainView implements Renderer {
+    public function render():string{
+        $html= '';
+        foreach($this->data as $image){
+        $html .= <<<EOT
+        <article class='image-article'>
+            <div>
+                <img alt="" src="https://webetu.iutnc.univ-lorraine.fr/www/pierso112u/Atelier_1/img/$image-%3Eid.jpg%22%3E
+            </div>
+        </article>
+        EOT;
+        }
+
+        return $html;
+    }
+}
