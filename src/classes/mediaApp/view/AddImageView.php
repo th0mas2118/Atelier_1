95e04e8d3bcd46514a3ef5d6d8fc86b7d12b0ae3
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-11-09 15:35:03 +0100

    Started upload handler

--- src/classes/mediaApp/view/AddImageView.php
+++ src/classes/mediaApp/view/AddImageView.php
@@ -4,11 +4,13 @@ namespace iutnc\mediaApp\view;
 
 use \iutnc\mf\view\Renderer;
 
-class AddImageView extends MainView implements Renderer{
-    public function render():string{
-        $res=<<<EOT
+class AddImageView extends MainView implements Renderer
+{
+    public function render(): string
+    {
+        $res = <<<EOT
             <section>
-                <form method=post>
+                <form method="post" enctype="multipart/form-data">
                 <label for="title">Title : </label>
                 <input class="forms-text" type=text name=title placeholder="title">
                 <label for="description">Description : </label>
@@ -18,6 +20,7 @@ class AddImageView extends MainView implements Renderer{
 
                 <label for="img">Choose a new picture to add:</label>
                 <input type="file" id="img" name="img" accept="image/jpeg">
+                <input type="hidden" name="galleryId" value="your_custom_value">
                 <button class="forms-button" name=add_button type="submit"> Confirm </button>
                 </form>
             </section>
