6b96fea6401c934548211bece49e43f716bf1867
SEBonduel <sebastien050599@hotmail.fr>
2022-11-09 13:47:55 +0100

    fix synthax

--- src/classes/mediaApp/view/AddImageView.php
+++ src/classes/mediaApp/view/AddImageView.php
@@ -9,16 +9,16 @@ class AddImageView extends MainView implements Renderer{
         $res=<<<EOT
             <section>
                 <form method=post>
-                <label for="title">Title</label>
+                <label for="title">Title : </label>
                 <input class="forms-text" type=text name=title placeholder="title">
-                <label for="description">Description</label>
+                <label for="description">Description : </label>
                 <input class="forms-text" type=text name=Description placeholder="description">
-                <label for="Keywords">Keywords</label>
+                <label for="Keywords">Keywords : </label>
                 <input class="forms-text" type=text name=Keywords placeholder="keywords">
 
                 <label for="img">Choose a new picture to add:</label>
                 <input type="file" id="img" name="img" accept="image/jpeg">
-                <button class="forms-button" name=add_button type="submit">Submit</button>
+                <button class="forms-button" name=add_button type="submit"> Confirm </button>
                 </form>
             </section>
             EOT;
