2740e3895adeca7c6f597d456eef992b4c1c402e
th0mas2118 <thomaspierson32@gmail.com>
2022-11-09 09:35:33 +0100

    Add SignUpView

---
+++ src/classes/mediaApp/control/SignUpController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace iutnc\mediaApp\control;
+
+use \iutnc\mf\control\AbstractController;
+use \iutnc\mediaApp\view\SignUpView;
+
+class SignUpController extends AbstractController{
+    public function execute():void{
+        
+    }
+}
---
+++ src/classes/mediaApp/view/SignUpView.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace iutnc\mediaApp\view;
+
+use \iutnc\mf\view\Renderer;
+
+class SignUpView extends MainView implements Renderer{
+    public function render():string{
+        $res=<<<EOT
+        EOT;
+        return $res;
+    }
+}
