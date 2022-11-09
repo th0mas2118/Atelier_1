9b025d39cb1187893d3b1f6339fabdcf7a193526
th0mas2118 <thomaspierson32@gmail.com>
2022-11-09 10:18:33 +0100

    add imageView

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ $router->addRoute('user', 'user_galleries', '\iutnc\mediaApp\control\UserControl
 $router->addRoute('gallery', 'show_gallery', '\iutnc\mediaApp\control\GalleryController');
 $router->addRoute('signup', 'signup', '\iutnc\mediaApp\control\SignUpController');
 $router->addRoute('new_gallley', 'new_gallery', '\iutnc\mediaApp\control\CreateGalleryController');
+$router->addRoute('image', 'image', '\iutnc\mediaApp\control\ImageController');
 
 
 $router->setDefaultroute('home');
