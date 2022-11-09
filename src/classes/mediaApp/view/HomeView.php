3ad33f0f93247a8c9a5dbc029a6c6406cc6626ef
th0mas2118 <thomaspierson32@gmail.com>
2022-11-09 14:09:50 +0100

    add random from gallery

--- src/classes/mediaApp/view/HomeView.php
+++ src/classes/mediaApp/view/HomeView.php
@@ -3,6 +3,12 @@
 namespace iutnc\mediaApp\view;
 
 use \iutnc\mf\view\Renderer;
+use \iutnc\meadiaApp\model\Image;
+
+function jsonPrint($printable){
+    $json=json_encode($printable,JSON_PRETTY_PRINT);
+    return "<pre>{$json}</pre>";
+}
 
 class Homeview extends MainView implements Renderer{
     public function render():string{
@@ -10,23 +16,28 @@ class Homeview extends MainView implements Renderer{
         foreach($this->data as $g){
             $name=$g->name()->first()['fullname'];
             $nb_image=$g->nb_images();
-            $random=rand(206,$nb_image);
-            $img_src='https://webetu.iutnc.univ-lorraine.fr/www/pierso112u/Atelier_1/img/thumbnails/'.$random.'.jpg';
-            $res.=<<<EOT
-                <article class='gallery-article'>
-                    <div>
-                        <h3>{$g['name']}</h3>
+            $image_list=$g->images()->get();
+            $count=count($image_list);
+            $random=rand(0,$count-1);
+            if($count>0){
+                $index=$image_list[$random]->id;
+                $img_src='https://webetu.iutnc.univ-lorraine.fr/www/pierso112u/Atelier_1/img/thumbnails/'.$index.'.jpg';
+                $res.=<<<EOT
+                    <article class='gallery-article'>
                         <div>
-                            <img alt='image' src=$img_src></img>
+                            <h3>{$g['name']}</h3>
+                            <div>
+                                <img alt='image' src=$img_src></img>
+                            </div>
+                            <div>
+                                <span>{$nb_image}</span>
+                                <span>$name</span>
+                                <span>{$g['created_at']}</span>
+                            </div>
                         </div>
-                        <div>
-                            <span>{$nb_image}</span>
-                            <span>$name</span>
-                            <span>{$g['created_at']}</span>
-                        </div>
-                    </div>
-                </article>
+                    </article>
             EOT;
+            }
         }
         return $res;
     }
