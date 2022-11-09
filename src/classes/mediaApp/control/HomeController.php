cf5cffe95caaef0e518eb26026e53a50ff30a9a2
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-11-09 15:46:40 +0100

    limit to 20 galleries for homepage

--- src/classes/mediaApp/control/HomeController.php
+++ src/classes/mediaApp/control/HomeController.php
@@ -6,10 +6,12 @@ use \iutnc\mf\control\AbstractController;
 use \iutnc\mediaApp\model\Gallery;
 use \iutnc\mediaApp\view\HomeView;
 
-class HomeController extends AbstractController{
-    public function execute():void{
-        $gl=Gallery::select()->get();
-        $hv=new HomeView($gl);
+class HomeController extends AbstractController
+{
+    public function execute(): void
+    {
+        $gl = Gallery::select()->take(20)->get();
+        $hv = new HomeView($gl);
         $hv->makePage();
     }
 }
