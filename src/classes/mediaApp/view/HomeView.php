d8641d0ece80d97b73ade5cbb1db1ca83b66813b
th0mas2118 <thomaspierson32@gmail.com>
2022-11-09 14:39:55 +0100

    add click to gallery

--- src/classes/mediaApp/view/HomeView.php
+++ src/classes/mediaApp/view/HomeView.php
@@ -14,22 +14,25 @@ class Homeview extends MainView implements Renderer{
             $image_list=$g->images()->get();
             $count=count($image_list);
             $random=rand(0,$count-1);
+            $url_gallery='https://webetu.iutnc.univ-lorraine.fr/www/pierso112u/Atelier_1/?action=gallery&id='.$g->id;
             if($count>0){
                 $index=$image_list[$random]->id;
                 $img_src='https://webetu.iutnc.univ-lorraine.fr/www/pierso112u/Atelier_1/img/thumbnails/'.$index.'.jpg';
                 $res.=<<<EOT
                     <article class='gallery-article'>
+                        <a href={$url_gallery}>
                         <div>
-                            <h3>{$g['name']}</h3>
-                            <div>
-                                <img alt='image' src=$img_src></img>
-                            </div>
-                            <div>
-                                <span>{$nb_image}</span>
-                                <span>$name</span>
-                                <span>{$g['created_at']}</span>
-                            </div>
+                        <h3>{$g['name']}</h3>
+                        <div>
+                            <img alt='image' src=$img_src></img>
                         </div>
+                        <div>
+                            <span>{$nb_image}</span>
+                            <span>$name</span>
+                            <span>{$g['created_at']}</span>
+                        </div>
+                        </div>
+                        </a>
                     </article>
             EOT;
             }
