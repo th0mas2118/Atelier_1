43427a69576ac8d138d9e50642419c8a8f9a8061
Asley <asleyvalisoa@gmail.com>
2022-11-09 13:19:23 +0200

    Update AddImageController.php

--- src/classes/mediaApp/control/AddImageController.php
+++ src/classes/mediaApp/control/AddImageController.php
@@ -8,15 +8,6 @@ use \iutnc\mf\router\Router;
 
 class AddImageController extends AbstractController{
 
-    /* Algorithme de execute:
- *
- *  1 Récupérer tous les tweets en utilisant le modèle Tweet
- *  2 Parcourir le résultat
- *      construire la chaine HTML qui contien :
- *      le text du tweet, l'auteur et la date de création
- *  3 Retourner un block HTML qui met en forme la liste
- *
- */
 
     public function execute():void{
         if($this->request->method==='GET'){
