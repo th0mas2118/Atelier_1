531e311c0cf9cdc0206dd300782f9ba8ccd44616
SEBonduel <sebastien050599@hotmail.fr>
2022-11-09 14:39:00 +0100

    add github to aboutView

--- src/classes/mediaApp/view/AboutView.php
+++ src/classes/mediaApp/view/AboutView.php
@@ -8,7 +8,7 @@ class AboutView extends MainView implements Renderer{
     public function render():string{
         $res=<<<EOT
             <section>
-               QUEUE
+               <a href="https://github.com/th0mas2118/Atelier_1/">GitHub de l'atelier</a>
             </section>
             EOT;
         return $res;
