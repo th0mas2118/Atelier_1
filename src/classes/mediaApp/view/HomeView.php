c6852a6a04c160173dcc255df548a9084bc2b048
Asley <asleyvalisoa@gmail.com>
2022-11-10 16:31:05 +0200

    update pagination

--- src/classes/mediaApp/view/HomeView.php
+++ src/classes/mediaApp/view/HomeView.php
@@ -12,7 +12,7 @@ class Homeview extends MainView implements Renderer
     {
         $res = "<section id='home-list'>";
 
-        foreach ($this->data as $g) {
+        foreach ($this->data["galleryList"] as $g) {
             $name = $g->name()->first()['fullname'];
             $nb_image = $g->nb_images();
             $image_list = $g->images()->get();
@@ -46,15 +46,17 @@ class Homeview extends MainView implements Renderer
         $page = isset($this->request->get['page']) && !empty($this->request->get['page']) ? $this->request->get['page'] : 1;
         $previousPage = $page-1;
         $nextPage = $page + 1;
+        $nbrePage = 20;
+        $p = ceil($this->data["nombreArticle"]/$nbrePage);
         $btnP = $page>1 ? "<a href='#'><i class='fa-solid fa-angles-left'></i></a>
                 <a href='?action=home&page={$previousPage}'><i class='fa-solid fa-chevron-left'></i></a>" : '';
         $btnN = 
         $res .= "<section id='gallery'>
             <div class='pagination'>
                 {$btnP}
-                <a href='#'>1</a>
+                <a>$page</a>
                 <a href='?action=home&page={$nextPage}'><i class='fa-solid fa-chevron-right'></i></a>
-                <a href='#'><i class='fa-solid fa-angles-right'></i></a>
+                <a href='?action=home&page={$p}''><i class='fa-solid fa-angles-right'></i></a>
             </div>
         </section>";
         
