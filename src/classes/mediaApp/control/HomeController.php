b0158ffaeb4199ab0d3acde978bc0dee815601b2
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-11-11 11:18:16 +0100

    homepage show most recents images first

--- src/classes/mediaApp/control/HomeController.php
+++ src/classes/mediaApp/control/HomeController.php
@@ -10,17 +10,16 @@ class HomeController extends AbstractController
 {
     public function execute(): void
     {
-            $gl ;
-            if(isset($this->request->get['page']) && !empty($this->request->get['page'])){
-                $page = $this->request->get['page'] - 1;
-                $gl=Gallery::select()->skip($page * 20)->take(20)->get();
-            }else{
-                $gl=Gallery::select()->take(20)->get();
-            }
-
-            $nbreArticle = Gallery::count();
-            $hv = new HomeView(["galleryList" => $gl, "nombreArticle"=>$nbreArticle]);
-            $hv->makePage();
+        $gl;
+        if (isset($this->request->get['page']) && !empty($this->request->get['page'])) {
+            $page = $this->request->get['page'] - 1;
+            $gl = Gallery::select()->orderBy('created_at', 'DESC')->skip($page * 20)->take(20)->get();
+        } else {
+            $gl = Gallery::select()->orderBy('created_at', 'DESC')->take(20)->get();
         }
-    }
 
+        $nbreArticle = Gallery::count();
+        $hv = new HomeView(["galleryList" => $gl, "nombreArticle" => $nbreArticle]);
+        $hv->makePage();
+    }
+}
