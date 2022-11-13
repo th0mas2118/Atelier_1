959b36ed671865326c59fd06598938b75c690b0f
th0mas2118 <71757813+th0mas2118@users.noreply.github.com>
2022-11-13 20:46:08 +0100

    add gallery description

--- src/classes/mediaApp/view/GalleryView.php
+++ src/classes/mediaApp/view/GalleryView.php
@@ -11,7 +11,14 @@ class GalleryView extends MainView implements Renderer
     public function render(): string
     {
         $title = $this->data['gallery']->name;
+        $keyword="";
         $html = "<h1>{$title}</h1>";
+        $html .= "<h3>Description : {$this->data['gallery']->description}</h3>";
+        foreach ($this->data['gallery']->keywords as $key) {
+            $keyword .= $key->content . ' ';
+        }
+        $html .="<span>Keywords :$keyword</span>";
+        $html .= "";
         $html .= '<section id="gallery-list">';
         $root = $this->request->root;
         foreach ($this->data['images'] as $image) {
