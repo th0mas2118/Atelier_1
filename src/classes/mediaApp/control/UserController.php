93427c0f38923b331e902baef5f5972e590f0e05
anthod57 <87667439+anthod57@users.noreply.github.com>
2022-11-14 16:08:55 +0100

    hide private galeries from user page

--- src/classes/mediaApp/control/UserController.php
+++ src/classes/mediaApp/control/UserController.php
@@ -2,6 +2,8 @@
 
 namespace iutnc\mediaApp\control;
 
+use iutnc\mediaApp\auth\Authentification;
+use iutnc\mediaApp\model\Gallery;
 use \iutnc\mediaApp\model\User;
 use \iutnc\mf\router\Router;
 use \iutnc\mf\auth\AbstractAuthentification;
@@ -11,8 +13,8 @@ class UserController extends \iutnc\mf\control\AbstractController
     public function execute($error = null): void
     {
         if (!isset($this->request->get['id'])) {
-            if (AbstractAuthentification::connectedUser()) {
-                $u = User::select()->where('id', '=', AbstractAuthentification::connectedUser())->first();
+            if (Authentification::connectedUser()) {
+                $u = User::select()->where('id', '=', Authentification::connectedUser())->first();
                 $uv = new \iutnc\mediaApp\view\UserView($u);
                 $uv->makePage();
                 return;
@@ -20,14 +22,29 @@ class UserController extends \iutnc\mf\control\AbstractController
             Router::executeRoute('home');
             return;
         }
+
         $id = $this->request->get['id'];
+
         if (is_null(User::find($id))) {
             Router::executeRoute('home');
             return;
         }
+
         $u = User::select()->where('id', '=', $id)->first();
-        //$g=$g->galleries()->get();
-        $uv = new \iutnc\mediaApp\view\UserView($u);
+
+        $gl = Gallery::where('hasImage', '=', true)->where('author', '=', $id);
+
+        if (!Authentification::connectedUser()) {
+            $gl = $gl->where('isPrivate', '=', false)->get();
+        } else {
+            $userAccess = User::where('id', '=', Authentification::connectedUser())->first()->access()->pluck('gallery_id')->toArray();
+
+            $gl = $gl->get()->filter(function ($k) use ($userAccess) {
+                return ($k['isPrivate'] == 0) || ($k['isPrivate'] == 1 && in_array($k['id'], $userAccess)) || ($k['author'] == Authentification::connectedUser());
+            });
+        }
+
+        $uv = new \iutnc\mediaApp\view\UserView(['user' => $u, 'galleries' => $gl]);
         $uv->makePage();
     }
 }
