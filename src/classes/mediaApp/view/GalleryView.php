89aa6ac6d339ffaac11cb770909d3329966f5c11
Asley <asleyvalisoa@gmail.com>
2022-11-14 09:45:20 +0200

    pagination gallery

--- src/classes/mediaApp/view/GalleryView.php
+++ src/classes/mediaApp/view/GalleryView.php
@@ -10,6 +10,7 @@ class GalleryView extends MainView implements Renderer
 {
     public function render(): string
     {
+        $url_gallery = $_GET['gallery_id'];
         $title = $this->data['gallery']->name;
         $keyword="";
         $html = "<h1>{$title}</h1>";
@@ -25,6 +26,22 @@ class GalleryView extends MainView implements Renderer
             $url_image = $this->router->urlFor('image', [['id', $image->id]]);
             $html .= RenderFunction::renderImage($image, $root, $url_image);
         }
+        $page = isset($this->request->get['page']) && !empty($this->request->get['page']) ? $this->request->get['page'] : 1;
+        $previousPage = $page - 1;
+        $nextPage = $page + 1;
+        $nbrePage = 20;
+        $pageInit = 0;
+        $p = ceil($this->data["nbreArticle"] / $nbrePage);
+        $btnP = $page > 1 ? "<a href='?action=gallery&gallery_id={$url_gallery}&page={$pageInit}'><i class='fa-solid fa-angles-left'></i></a>
+                <a href='?action=gallery&gallery_id={$url_gallery}&page={$previousPage}'><i class='fa-solid fa-chevron-left'></i></a>" : '';
+        $btnN = $page < $p ? "<a href='?action=gallery&gallery_id={$url_gallery}&page={$nextPage}'><i class='fa-solid fa-chevron-right'></i></a>
+        <a href='?action=gallery&gallery_id={$url_gallery}&page={$p}''><i class='fa-solid fa-angles-right'></i></a>" : '';
+            $html .= "<section id='gallery'>
+            <div class='pagination'>
+                {$btnP}
+                <a>$page</a>
+                {$btnN}
+            </div>";
         $html .= "</section>";
         return $html;
     }
